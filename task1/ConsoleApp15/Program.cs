e4cab283cb253ed67a48ddeb87fdaf3e21f15a8c
Savin Maksim <savin.maksim.u@gmail.com>
2024-07-13 00:22:21 +0500

    Пятое задание

--- task1/ConsoleApp15/Program.cs
+++ task1/ConsoleApp15/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 using System.Threading.Channels;
 
 namespace ConsoleApp15
@@ -6,7 +7,7 @@ namespace ConsoleApp15
     internal class Program
     {
         static string WrongChars(string text)
-        {
+        {                                           //Выявление неправильно введёных символов и склеивание в строку
             int count = 0;
             char[] chars = text.ToCharArray();
             char[] result = new char[chars.Length];
@@ -94,25 +95,159 @@ namespace ConsoleApp15
 
             return longestSubstring;
         }
+        public class QuickSortStrings
+        {
+            public static string Sort(string input)
+            {
+                char[] array = input.ToCharArray(); //Строка в массив символов
+                Sort(array, 0, array.Length - 1);
+                return new string(array);
+            }
+
+            private static void Sort(char[] array, int left, int right) 
+            {
+                if (left < right)
+                {
+                    int pivotIndex = Partition(array, left, right); //Быстрая сортировка на массиве символов
+                    Sort(array, left, pivotIndex - 1);
+                    Sort(array, pivotIndex + 1, right);
+                }
+            }
+
+            private static int Partition(char[] array, int left, int right) // Сортирует по алфавитному порядку с определением опорного элемента
+            {
+                char pivot = array[right];
+                int i = left - 1;
+
+                for (int j = left; j < right; j++)
+                {
+                    if (array[j] <= pivot)
+                    {
+                        i++;
+                        Swap(array, i, j); // Меняет местами два элемента массива
+                    }
+                }
+
+                Swap(array, i + 1, right);
+                return i + 1;
+            }
+
+            private static void Swap(char[] array, int a, int b)
+            {
+                char temp = array[a];
+                array[a] = array[b];
+                array[b] = temp;
+            }
+        }
+        public class TreeNode
+        {
+            public char Value; // Узел бинарного дерева поиска , содержит значение узла и ссылки на поддеревья
+            public TreeNode Left;
+            public TreeNode Right;
+
+            public TreeNode(char value)
+            {
+                Value = value;
+                Left = null;
+                Right = null;
+            }
+        }
+
+        public class TreeSort
+        {
+            public static string Sort(string input) //Вставляет каждый символ строки в бинарное дерево
+            {
+                
+
+                TreeNode root = null;
+                foreach (char c in input)
+                {
+                    root = Insert(root, c);
+                }
+
+                string sortedString = InOrderTraversal(root);
+                return sortedString;
+            }
+
+            private static TreeNode Insert(TreeNode node, char value) // Вставка нового узла
+            {
+                if (node == null)
+                {
+                    return new TreeNode(value);
+                }
+
+                if (value < node.Value)
+                {
+                    node.Left = Insert(node.Left, value);
+                }
+                else
+                {
+                    node.Right = Insert(node.Right, value);
+                }
+
+                return node;
+            }
+
+            private static string InOrderTraversal(TreeNode node) // Обход дерева в порядке in-order-travelsal
+            {
+                if (node == null)
+                {
+                    return string.Empty;
+                }
+
+                string left = InOrderTraversal(node.Left);
+                string right = InOrderTraversal(node.Right);
+
+                return left + node.Value + right;
+            }
+        }
         static void Main(string[] args)
         {
-            string s = Console.ReadLine(); //Вход данных
-            if (IsOnlyLetters_Method(s) == true) //Проверка на введение символов
+            while (true)
             {
-                Console.WriteLine(ReversString(s)); // Переворот строки
-                var result = NumberOfOccurrences(s); //Получение количества вхождений символов
-                foreach (var item in result)
+                Console.Clear();
+                string s = Console.ReadLine(); //Вход данных
+                if (IsOnlyLetters_Method(s) == true) //Проверка на введение символов
                 {
-                    Console.WriteLine($"Символ: {item.Key}, Количество: {item.Value}"); // Вывод количества символов 
+                    Console.WriteLine(ReversString(s)); // Переворот строки
+                    var result = NumberOfOccurrences(s); //Получение количества вхождений символов
+                    foreach (var item in result)
+                    {
+                        Console.WriteLine($"Символ: {item.Key}, Количество: {item.Value}"); // Вывод количества символов 
+                    }
+                    Console.WriteLine(FindLongestVowelSubstring(ReversString(s))); //Поиск самой длинной подстроки по гласным
+                    Console.WriteLine("Выберите метод сортировки:");
+                    Console.WriteLine("1. Быстрая сортировка (Quick Sort)");
+                    Console.WriteLine("2. Сортировка деревом (Tree Sort)");
+                    int choice = int.Parse(Console.ReadLine());
+
+                    switch (choice) // Выбор метода сортировки
+                    {
+                        case 1:
+                            QuickSortStrings.Sort(ReversString(s));
+                            Console.WriteLine("Отсортировано с помощью быстрой сортировки:" + QuickSortStrings.Sort(s));
+                            break;
+                        case 2:
+                            TreeSort.Sort(ReversString(s));
+                            Console.WriteLine("Отсортировано с помощью сортировки деревом:" + TreeSort.Sort(s));
+                            break;
+                        default:
+                            Console.WriteLine("Неверный выбор");
+                            return;
+                    }
                 }
-                Console.WriteLine(FindLongestVowelSubstring(ReversString(s)));
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("Введены некорректные символы : " + WrongChars(s));
-                
-                Console.ReadKey();
+                else
+                {
+                    Console.WriteLine("Введены некорректные символы : " + WrongChars(s));
+
+                    Console.ReadKey();
+                }
+                Console.WriteLine("Хотите перезапустить программу? (y - Да/n - Нет)");
+                string restartChoice = Console.ReadLine().ToLower();
+                if (restartChoice != "y")
+                {
+                    break;
+                }
             }
         }
     }
